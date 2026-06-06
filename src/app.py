41c9a7fa52460162a75b80f3e1ab0cd210914343
Emanueli Cristina <cristinagodoy89br@gmail.com>
2026-06-06 17:19:36 +0000

    API lista tareas

---
+++ src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+todos = [
+    { "label": "My first task", "done": False }
+]
+
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+
+   
+    todos.append(request_body)
+
+    
+    return jsonify(todos)
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    # eliminar el elemento por índice
+    todos.pop(position)
+
+   
+    return jsonify(todos)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
